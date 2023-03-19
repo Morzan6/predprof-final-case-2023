75371ace5a276e766e17d77b2b4c453e1c29be07
Morzan6 <n.kusmaul@yandex.ru>
2023-03-19 10:25:04 +0300

    modified:   space_farm/get_zapr.py

--- space_farm/get_zapr.py
+++ space_farm/get_zapr.py
@@ -1,5 +1,9 @@
 import requests
 
+key  = "9e83w26h"
+
+
+adr = "https://dt.miet.ru/ppo_it_final"
 def req(adr, key):
 
     params = {"X-Auth-Token": key}
@@ -7,3 +11,5 @@ def req(adr, key):
     resp = requests.get(adr, headers=params).text
 
     return resp
+
+print(req(adr, key))
