0a5252ffc4e8d71808150365e21fbdd7d2e8358f
Morzan6 <n.kusmaul@yandex.ru>
2023-03-19 10:09:42 +0300

    new file:   space_farm/config/__pycache__/__init__.cpython-310.pyc 	new file:   space_farm/config/__pycache__/settings.cpython-310.pyc 	new file:   space_farm/config/__pycache__/urls.cpython-310.pyc 	new file:   space_farm/config/__pycache__/wsgi.cpython-310.pyc 	modified:   space_farm/config/settings.py 	new file:   space_farm/db.sqlite3

--- space_farm/config/settings.py
+++ space_farm/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'space_farm.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'space_farm.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
