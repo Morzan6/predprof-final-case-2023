1839b9a66ee35c983f321cb41e5cf57343705e5e
Sparky324 <98699424+Sparky324@users.noreply.github.com>
2023-03-19 12:56:56 +0300

    Update formuls.py

--- space_farm/core/scripts/formuls.py
+++ space_farm/core/scripts/formuls.py
@@ -3,18 +3,18 @@ from math import sin, pi
 #1 единица топлива стоит 10 кредитов, одна единица кислорода стоит 7 кредитов
 
 
-def V(W, Sh):   #Максимальная скорость, Мощность, Масса
-    return 2*(W/80)*(200/(192+Sh))
+def V(W, Sh, Oxi, T):   #Максимальная скорость, Мощность, Масса
+    try:
+        return 2*(W/80)*(200/(192+Sh+Sh*sin(-(pi/2)+(pi*(T+0.5*(Oxi/Sh)))/(40))))
+    except:
+        return 2
 
 
-def SHnew(G0, Kp): #новое значение Sh
-    return G0 + G0*Kp
-
-
-
-def Kp(Oxi, T):
-    #Oxi <= 60!!
-    return sin(-(pi/2)+(pi*(T+0.5*Oxi))/(40))
+def SHnew(G0, T, Oxi): #новое значение Sh
+    try:
+        return G0 + G0*sin(-(pi/2)+(pi*(T+0.5*(Oxi/G0)))/(40))
+    except:
+        return 30
 
 
 def E(T):
