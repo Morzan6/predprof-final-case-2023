8f3daa01e33368d07bd5bee345f98b6cbd3ae9e0
Morzan6 <n.kusmaul@yandex.ru>
2023-03-19 12:12:09 +0300

    modified:   space_farm/config/__pycache__/settings.cpython-310.pyc 	modified:   space_farm/config/settings.py 	new file:   space_farm/core/__pycache__/admin.cpython-310.pyc 	new file:   space_farm/core/__pycache__/apps.cpython-310.pyc 	new file:   space_farm/core/__pycache__/models.cpython-310.pyc 	modified:   space_farm/core/__pycache__/views.cpython-310.pyc 	new file:   space_farm/core/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   space_farm/core/templates/scenario.html 	new file:   space_farm/core/templatetags/__pycache__/delay.cpython-310.pyc 	new file:   space_farm/core/templatetags/__pycache__/delay_cycle.cpython-310.pyc 	new file:   space_farm/core/templatetags/__pycache__/delayed_cycle.cpython-310.pyc 	new file:   space_farm/core/templatetags/__pycache__/loop.cpython-310.pyc 	new file:   space_farm/core/templatetags/delay.py 	modified:   space_farm/core/views.py

--- space_farm/core/views.py
+++ space_farm/core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+import datetime
 
 def index(request):
-    return render(request,'scenario.html')
+    days = [1, 2, 3, 4, 5]
+    days_time = 2000
+    now = datetime.datetime.now()
+    return render(request,'scenario.html', {'now': now, "days": days, "days_time": days_time})
